68d93fb4db7ffca799970c12e0902d9aba6339fc
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-11-01 13:22:35 -0300

    User SignUp

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response} from 'express'
 import {body, validationResult} from 'express-validator'
+import {User} from '../models/user'
 import {RequestValidationError} from '../errors/request-validation-error'
-import {DatabaseConnectionError} from '../errors/database-connection-error'
+import { BadRequestError } from '../errors/bad-request-erros'
 
 const router = express.Router()
 
@@ -14,16 +15,23 @@ router.post('/api/users/signup',[
         .isLength({min: 4, max: 15})
         .withMessage('Password must be between 4 and 15 characters')
     ],
-     (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
          const errors = validationResult(req)
          if (!errors.isEmpty()) {
              throw new RequestValidationError(errors.array())
          }
-    const {email, password} = req.body
-    console.log('Creating user for ', email)
-    throw new DatabaseConnectionError()
+        const {email, password} = req.body;
 
-    res.send({})
-});
+        const existingUser = await User.findOne({email})
+        if (existingUser) {
+            throw new BadRequestError('Email in use')
+        }
+
+        const user = User.build({email, password})
+        await user.save()
+
+        res.status(201).send(user)
+    }
+);
 
 export {router as signupRouter}
